7c6080bf2ff908da05b66b7a3b05ca128bdb334d
kunalshimpi <kunal_shimpi@persistent.co.in>
2017-03-03 09:46:48 +0530

    second commit

--- chaincode/chaincode_healthInsurance.go
+++ chaincode/chaincode_healthInsurance.go
@@ -23,9 +23,10 @@ import (
 	//"github.com/hyperledger/fabric/core/crypto/primitives"
 )
 
-// SimpleHealthChaincode example simple Chaincode implementation
-type SimpleHealthChaincode struct {
+
+type SimpleRewardsChaincode struct {
 }
+
 type eReward struct{
 	Points string `json:"points"`
 	Hash string `json:"hash"`
@@ -37,14 +38,14 @@ type eReward struct{
 // ============================================================================================================================
 func main() {
 	//primitives.SetSecurityLevel("SHA", 256)
-	err := shim.Start(new(SimpleHealthChaincode))
+	err := shim.Start(new(SimpleRewardsChaincode))
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
 }
 
 // Init resets all the things
-func (t *SimpleHealthChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleRewardsChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("**********Inside Init*******");
 	if len(args) != 0 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 0")
@@ -76,29 +77,31 @@ func (t *SimpleHealthChaincode) Init(stub shim.ChaincodeStubInterface, function 
 	return nil, nil
 }
 
-func (t *SimpleHealthChaincode) assign(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleRewardsChaincode) assign(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	fmt.Println("assign is running ")
 
 	if len(args) != 3{
 		return nil, errors.New("Expected 3 arguments!")
 	}
 
-	points, _ := strconv.Atoi(args[1]) //parse to int
-	user := args[0]
+	points, _ := strconv.Atoi(args[0]) //parse to int
+	user := args[1]
 	Sign_assigner := args[2]
 
 	eRewardAsBytes, err := stub.GetState(user)
 	if err != nil {
 		return nil, errors.New("Failed to get eReward Object")
 	}
+
 	if eRewardAsBytes == nil {
 		t.init_eReward(stub, args) //will create key/value with eReward stuct
 	}else{
-		//update existing eReward struct
+	//update existing eReward struct
 	eRewardAsBytes, err := stub.GetState(user)
 	if err != nil {
 		return nil, errors.New("Failed to get struct")
 	}
+
 	res := eReward{}
 	json.Unmarshal(eRewardAsBytes, &res)
 
@@ -163,15 +166,15 @@ func (t *SimpleHealthChaincode) assign(stub shim.ChaincodeStubInterface, args []
 	return nil, err
 }
 
-func (t *SimpleHealthChaincode)init_eReward(stub shim.ChaincodeStubInterface, args []string)([]byte, error){
+func (t *SimpleRewardsChaincode)init_eReward(stub shim.ChaincodeStubInterface, args []string)([]byte, error){
 	fmt.Println("init_eReward is running ")
 
 	if len(args) != 3{
 		return nil, errors.New("Expected 3 arguments!")
 	}
 
-	points, _ := strconv.Atoi(args[1]) //parse to int
-	user := args[0]
+	points, _ := strconv.Atoi(args[0]) //parse to int
+	user := args[1]
 	Sign_assigner := args[2]
 
 	err := stub.CreateTable("ActivityTable", []*shim.ColumnDefinition{
@@ -195,10 +198,10 @@ func (t *SimpleHealthChaincode)init_eReward(stub shim.ChaincodeStubInterface, ar
 
 	objAsJson, err1 := json.Marshal(obj)
 	if err1 !=nil {
-		return nil,err1
+		return nil, err1
 	}
 
-	err = stub.PutState("struct", objAsJson)
+	err = stub.PutState(user, objAsJson)
 	if err != nil {
 		return nil, err
   }
@@ -221,13 +224,13 @@ func (t *SimpleHealthChaincode)init_eReward(stub shim.ChaincodeStubInterface, ar
 	return nil,nil
 }
 
-func (t *SimpleHealthChaincode) redeem(stub shim.ChaincodeStubInterface, args []string)([]byte, error){
+func (t *SimpleRewardsChaincode) redeem(stub shim.ChaincodeStubInterface, args []string)([]byte, error){
 	if len(args) != 3{
 		return nil, errors.New("Expected 3 arguments!")
 	}
-	b_entity := args[0]
+	redeemPoints,_ := strconv.Atoi(args[0])
 	user := args[1]
-	redeemPoints,_ := strconv.Atoi(args[2])
+	b_entity := args[2]
 
 	/*
 //check caller and Owner
@@ -239,11 +242,12 @@ func (t *SimpleHealthChaincode) redeem(stub shim.ChaincodeStubInterface, args []
 		return nil, errors.New("The caller is not the owner of the amount")
 	}*/
 
-//change assets  of sender
+//redeem points from user
 	eRewardAsBytes, err := stub.GetState(user)
 	if err != nil {
  		return nil, errors.New("Failed to get struct")
 	}
+
 	res := eReward{}
 	json.Unmarshal(eRewardAsBytes, &res)
 
@@ -276,7 +280,7 @@ func (t *SimpleHealthChaincode) redeem(stub shim.ChaincodeStubInterface, args []
 	return nil, nil
 }
 
-func (t *SimpleHealthChaincode) isCaller(stub shim.ChaincodeStubInterface, certificate []byte) (bool, error) {
+func (t *SimpleRewardsChaincode) isCaller(stub shim.ChaincodeStubInterface, certificate []byte) (bool, error) {
 	// Verify \sigma=Sign(certificate.sk, tx.Payload||tx.Binding) against certificate.vk
 	fmt.Println("isCaller is Running!")
 
@@ -314,53 +318,37 @@ func (t *SimpleHealthChaincode) isCaller(stub shim.ChaincodeStubInterface, certi
 	return ok, err
 }
 
-
-// Invoke is our entry point to invoke a chaincode function
-func (t *SimpleHealthChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleRewardsChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
-	// Handle different functions
-	if function == "assign" {													//initialize the chaincode state, used as reset
+	if function == "assign" {
 		return t.assign(stub, args)
 	} else if function == "redeem"{
 		return t.redeem(stub, args)
 	}
-	fmt.Println("invoke did not find func: " + function)					//error
+	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
-// Query is our entry point for queries
-func (t *SimpleHealthChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+func (t *SimpleRewardsChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
-	// Handle different functions
-	if function == "read" {											//read a variable
+	if function == "read" {
 		return t.read(stub, args)
 	}
-	fmt.Println("query did not find func: " + function)						//error
+	fmt.Println("query did not find func: " + function)
 
 	return nil, errors.New("Received unknown function query: " + function)
 }
 
-func (t *SimpleHealthChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
+func (t *SimpleRewardsChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
 
 	if len(args) != 1 {
 		return nil, errors.New("Expected 1 argument!")
 	}
 	user := args[0]
-	//fmt.Println("Finding [%x]",string(applicant))
-/*
-	var columns []shim.Column
-	col := shim.Column{Value: &shim.Column_String{String: user}}
-	columns = append(columns,col)
 
-	row, err := stub.GetRow("InsuranceAmount",columns)
-	while(row != nil)
-	if err != nil {
-		return nil, errors.New("Cannot retrieve Rows")
-	}
-*/
 	valAsbytes, err := stub.GetState(user)									//get the var from chaincode state
 	if err != nil {
 		jsonResp := "{\"Error\":\"Failed to get state for " + user + "\"}"
@@ -376,10 +364,5 @@ func (t *SimpleHealthChaincode) read(stub shim.ChaincodeStubInterface, args []st
 	fmt.Printf("Query Response:%s\n", jsonResp)
 	fmt.Println("Finished Query function")
 	return valAsbytes, nil
-
-
-	//rowString := fmt.Sprintf("%s", row)
-	//return []byte(rowString), nil
-	//return row.Columns[0].GetBytes(), nil
 
 }
